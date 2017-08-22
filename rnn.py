0cd4ce42956ee6d0d4c7de4965c27cb9f7e9cf8b
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-08-22 16:11:34 +0200

    Data Processing for Training

--- rnn.py
+++ rnn.py
@@ -1,12 +1,16 @@
 """ Reconstruction of regulatory networks from sc-RNA seq Data using Recurrent Neural Networks with Long Short Term Memory  
 
-    Basic Architecture for RNN for reconstructing regulatory networks                                                          """
+    1. Baseline Method using RNN without Long Short Term Memory  -- Vanilla RNN                                                     """
+
 
 from __future__ import  division
 import numpy as np 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from noise_reduction import clustered_state, matrix_pass
 
+
+#Initialisations for Test
 num_epochs = 100
 total_series_length = 50000
 truncated_backprop_length = 15
@@ -17,7 +21,7 @@ batch_size = 5
 num_batches = total_series_length//batch_size//truncated_backprop_length
 
 
-#Generation of Data for the Given Problem
+#Function to Generate Data for the Testing the Network -- Type of Data : Rolled Sequence
 def generateData():
     #Generation of numbers from np.arange(2) with a size of total_series_length, probability of drawing a number is 0.5
     x = np.array(np.random.choice(2, total_series_length, p=[0.5, 0.5]))
@@ -33,6 +37,83 @@ def generateData():
     y = y.reshape((batch_size, -1))
 
     return (x, y)
+
+
+#Function to curate the data for the network
+def get_expression_data():
+    #Generate a time series dataset, parameter : Number of States
+    data_matrix, cells = matrix_pass()
+
+    data_matrix = data_matrix.transpose()
+
+    """ Expression levels of the first state are given as input """
+
+    #Input to the Recurrent Neural Network
+    input_data = data_matrix[0]
+
+    #Generate the Labels
+    labels = data_matrix[1:]
+
+    return input_data, labels
+
+
+#Function to implement the Recurrent Net for Modelling the Expression
+def recurrent_net():
+    #Training Data
+    input_matrix , labels = get_expression_data()
+
+    #Initial State of the Network will be the expression level of the first state -- To be reshaped
+    current_state = input_matrix
+
+    #Number of Neurons in Each State --> Number of Genes in one state
+    state_size = len(current_state)
+
+    #Reshaping the current matrix for matrix multiplication
+    current_state = np.reshape(current_state,(1,100))    
+
+    #Define the TensorFlow Variables for State Transitions
+    W = tf.Variable(np.random.rand(state_size,state_size),dtype=tf.float32)
+    b = tf.Variable(np.zeros((1,state_size)),dtype=tf.float32)
+
+    #Define the Tensorflow Variables for Output and Training
+    W2 = tf.Variable(np.random.rand(state_size,state_size),dtype=tf.float32)
+    b2 = tf.Variable(np.zeros((1,state_size)),dtype=tf.float32)
+
+    #Define the placeholder for state - Dimension : (1 * 100)
+    initial_state = tf.placeholder(tf.float32,[1,state_size])
+
+    #Start the Tensorflow Session
+    with tf.Session() as sess:
+        #Initialize all tensorflow variables
+        sess.run(tf.initialize_all_variables())
+
+        for i in range(num_epochs):
+            #In each epoch train the whole network
+            print len(sess.run(W2))
+
+            print initial_state.get_shape()
+
+            print current_state
+
+            break
+
+        
+
+
+
+
+
+
+
+recurrent_net()
+
+
+    
+
+
+#Input Data & Labels
+input_data,labels = get_expression_data()
+
 
 
 
@@ -92,7 +173,7 @@ total_loss = tf.reduce_mean(losses)
 train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)
 
 
-
+"""
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     loss_list = []
@@ -143,7 +224,7 @@ with tf.Session() as sess:
 
         
 
-
+"""
 
 
 
