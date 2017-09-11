cf26c787c0e826cabdc937c02a684009a448b148
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-09-11 10:38:06 +0200

    Training a model to predict labels for cells

---
+++ Latent/reduce.py
@@ -0,0 +1,3 @@
+""" Generation of Latent Representation of Cells by Machine Learning Methods 
+      Basic Intuition : Train a Model to accurately predict the cell labels across given the expression labels     """
+
