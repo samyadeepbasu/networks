f1e711ed76ba2f94e05c3406d91a7e41dd7fbe01
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-11-06 12:09:31 +0100

    Updated algorithm

--- transition_bias.py
+++ transition_bias.py
@@ -50,11 +50,10 @@ def create_data_matrix():
 
 
 
-
 #Function to get the ground truth for the dataset
 def ground_truth():
 	#Open and Initialise the File
-	g_file = open('ground_truth/stamlab_for_data3.txt','r')
+	g_file = open('ground_truth/stamlab_for_data2.txt','r')
 
 	#Conversion of the interactions in appropriate format  -- (Regulator --->  Target)
 	interactions = [ (int(line.split()[3]),int(line.split()[2])) for line in g_file.readlines()]
@@ -64,6 +63,173 @@ def ground_truth():
 			interactions.remove(item)
 	
 	return interactions
+
+
+#Function to find the unique transcriptional factors 
+def find_unique_tfs(interactions):
+	temp = []
+
+	for interaction in interactions:
+		temp.append(interaction[0])
+		temp.append(interaction[1])
+
+	temp = list(set(temp))
+
+	return temp
+
+
+#Function to generate the negative samples
+def get_negative_interactions(positive_interactions):
+	#Find the unique transcription factors
+	unique_tfs = find_unique_tfs(positive_interactions)
+
+	total_sample = []
+
+	for regulator in unique_tfs:
+		for target in unique_tfs:
+			if target != regulator:
+				total_sample.append((regulator,target))
+
+
+	negative_samples = list(set(total_sample) - set(positive_interactions))
+
+	return total_sample, negative_samples
+
+#Function to split into training and testing sample
+def split(total_samples):
+	
+	number_edges = len(total_samples)
+	
+	number_testing = int(0.2*number_edges)
+
+	#Do a random shuffle
+	random.shuffle(total_samples)
+
+	chunks = []
+	
+	for i in range(0,len(total_samples),number_testing):
+		chunks.append(total_samples[i:i+number_testing])
+
+
+	return chunks	
+
+#Function to train the model using Stochastic Gradient Descent and Biasing via Transition Probability
+def train_model(W,targets,edge,lam):
+	#Get the sum of the predictions of all edges 
+	p = 0
+
+	p_positive = 0
+	p_negative = 0
+
+	for e in targets:
+		prediction = np.matmul(W,e[0].reshape(1,-1).transpose())[0][0]
+		p += prediction
+
+		if e[1]==0:
+			p_negative += prediction
+
+		elif e[1] == 1:
+			p_positive += prediction
+
+
+	if p_positive > p_negative:
+		#No regularization required
+		return 2*W
+
+	else: #This is the condition where regularization is required
+		positive_derivative = 0
+		negative_derivative = 0
+
+		X_positive = 0
+		X_negative = 0
+		X_total = 0
+
+		for e in targets:
+			X_total += e[0]
+			if e[1] == 1:
+				X_positive += e[0]
+
+
+			if e[1] == 0:
+				X_negative += e[0]
+
+
+		positive_derivative = (p*X_positive - p_positive*X_total) / (p*p)
+		negative_derivative = (p*X_negative - p_negative*X_total) / (p*p)
+
+		#Regularization term
+		regularization_term = positive_derivative - negative_derivative
+
+		gradient = 2*W[0] +  lam*regularization_term
+
+		return gradient.reshape(1,-1)
+
+
+
+	return 
+
+#Function to create the classification model 
+def create_classification_model(training_set,testing_set,expression_matrix):
+	#True classification labels
+	true_labels = [edge[2] for edge in testing_set]
+
+	edge_scores = []
+
+	regulators = list(set([edge[0] for edge in training_set]))
+
+	""" Go to each local region of the graph and train the model """
+	for regulator in regulators:
+		targets = [(expression_matrix[edge[1]],edge[2]) for edge in training_set if edge[0]==regulator]
+
+		X = [target[0] for target in targets]
+		Y = [target[1] for target in targets]
+
+		#Initialise the weight matrix
+		W = np.random.rand(1,len(X[0]))
+
+		epochs = 100
+
+		alpha = 0.001
+
+		#print np.matmul(W,X[0].reshape(1,-1).transpose())
+
+		for i in range(0,epochs):
+			#Do a SGD after each edge
+			for edge in targets:
+				#Update the weight
+				gradient = train_model(W,targets,edge,0.1)
+
+				W = W - alpha*gradient
+
+
+
+				break
+
+
+			#Compute Loss
+			
+
+
+
+
+
+			break
+
+
+		
+		
+
+		break
+
+
+
+
+	
+
+
+
+
+	return 
 
 
 #Function 
@@ -85,7 +251,40 @@ def main():
 	random.shuffle(positive_interactions)
 	random.shuffle(negative_interactions)
 
-	
+	positive_samples = [link + (1,) for link in positive_interactions]
+	negative_samples = [link + (0,) for link in negative_interactions]
+
+	total_samples = positive_samples + negative_samples
+
+	#Randomly Shuffle the samples
+	random.shuffle(total_samples)
+
+	#Gene Expression Matrix
+	main_matrix = data_matrix.copy()
+
+	#Split into Training and Testing Data
+	splitted_sample = split(total_samples)
+	splitted_sample = splitted_sample[:5]
+
+	for i in range(0,len(splitted_sample)):
+		testing_set = splitted_sample[i]
+		training_set_index = range(0,len(splitted_sample))
+		training_set_index.remove(i)
+
+		training_set = []
+
+		for index in training_set_index:
+			training_set += splitted_sample[index]
+
+
+		#auc, aupr,const_aupr = create_model(training_set,testing_set,main_matrix,10)
+		auc = create_classification_model(training_set,testing_set,main_matrix)
+
+		break
+
+
+
+
 
 	return
 
